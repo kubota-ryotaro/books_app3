75ed0707e7a8ace715184c92cc850af67ca38b52
“kubota-ryotaro” <kubota-ryotaro@gmo-am.jp>
2018-08-26 15:05:14 +0900

    Railsの教科書終了.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_26_050833) do
+
+  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "author"
+    t.string "picture"
+  end
 
 end
